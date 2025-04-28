2a51ae005d2c595daf317922d8e3b8d2327867e2
Akentar2001 <aqintar66@gmail.com>
2025-04-28 23:36:24 +0300

    add student

--- Codes/Backend/app/config.py
+++ Codes/Backend/app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:C16_2148850@localhost/quran_memorization_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:2148843@localhost/quran_memorization_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
